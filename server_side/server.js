f2deed5e9f92f99ab2d83f18922d349c0d177268
MartinJewski <martin.machajewski@gmx.de>
2020-04-29 12:24:29 +0200

    express installed

---
+++ server_side/server.js
@@ -0,0 +1,7 @@
+//this script contains simple server side calculations/processes
+//to retrieve and push/update date in the local SQLite database db_example.db
+//NodeJs + ExpressJs
+
+var express = require('express');
+
+var app = express();
